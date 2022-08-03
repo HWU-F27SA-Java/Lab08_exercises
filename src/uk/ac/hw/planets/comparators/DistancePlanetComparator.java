9be6ee7589b43d707961ab66408d548c683d9193
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:39:30 +0100

    Create DistancePlanetComparator.java

---
+++ src/uk/ac/hw/planets/comparators/DistancePlanetComparator.java
@@ -0,0 +1,20 @@
+/**
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.hw.planets.comparators;
+
+import java.util.Comparator;
+
+import uk.ac.hw.planets.entities.Planet;
+/**
+ * 
+ * @author Hadj
+ */
+public class DistancePlanetComparator implements Comparator<Planet> {
+  public int compare(Planet p1, Planet p2) {
+        
+        return 0;
+  }
+
+}
